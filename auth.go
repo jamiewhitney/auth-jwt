60c039dde9e49e08210b26d3298b91db1872f827
jamiewhitney <thejamiewhitney@gmail.com>
2022-10-25 15:38:30 +0100

    refactor

--- auth.go
+++ auth.go
@@ -1,72 +1,97 @@
 package auth
 
 import (
-	"context"
-	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
-	"github.com/auth0/go-jwt-middleware/v2/jwks"
-	"github.com/auth0/go-jwt-middleware/v2/validator"
-	"github.com/sirupsen/logrus"
+	"errors"
+	"fmt"
+	"github.com/MicahParks/keyfunc"
+	"github.com/golang-jwt/jwt/v4"
+	"log"
 	"net/http"
-	"net/url"
-	"os"
 	"strings"
-	"time"
 )
 
-type CustomClaims struct {
+var (
+	errInvalidScope = errors.New("invalid scope").Error()
+)
+
+type Authorizer struct {
+	Audience string
+	Scope    string
+	Issuer   string
+	Jwks     *keyfunc.JWKS
+}
+
+type MyCustomClaims struct {
 	Scope string `json:"scope"`
+	jwt.RegisteredClaims
 }
 
-// Validate does nothing for this example, but we need
-// it to satisfy validator.CustomClaims interface.
-func (c CustomClaims) Validate(ctx context.Context) error {
-	return nil
-}
-
-func EnsureValidToken(log *logrus.Logger) func(next http.Handler) http.Handler {
-	issuerURL, err := url.Parse("https://" + os.Getenv("AUTH0_DOMAIN") + "/")
+func NewAuthorizer(scope string, audience string, issuer string, jwksURL string) *Authorizer {
+	jwks, err := keyfunc.Get(jwksURL, keyfunc.Options{})
 	if err != nil {
-		log.Fatalf("failed to parse the issuer url: %v", err)
+		log.Fatalf("Failed to get the JWKS from the given URL. Error: %s", err)
 	}
 
-	provider := jwks.NewCachingProvider(issuerURL, 5*time.Minute)
-
-	jwtValidator, err := validator.New(
-		provider.KeyFunc,
-		validator.RS256,
-		issuerURL.String(),
-		[]string{os.Getenv("AUTH0_AUDIENCE")},
-		validator.WithCustomClaims(
-			func() validator.CustomClaims {
-				return &CustomClaims{}
-			},
-		),
-
-		validator.WithAllowedClockSkew(time.Minute),
-	)
-	if err != nil {
-		log.Fatalf("failed to set up the jwt validator")
-	}
-
-	errorHandler := func(w http.ResponseWriter, r *http.Request, err error) {
-		log.Printf("encountered error while validating JWT: %v", err)
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte(`{"message":"failed to validate JWT."}`))
-	}
-
-	middleware := jwtmiddleware.New(
-		jwtValidator.ValidateToken,
-		jwtmiddleware.WithErrorHandler(errorHandler),
-	)
-
-	return func(next http.Handler) http.Handler {
-		return middleware.CheckJWT(next)
+	return &Authorizer{
+		Audience: audience,
+		Scope:    scope,
+		Issuer:   issuer,
+		Jwks:     jwks,
 	}
 }
 
-func (c CustomClaims) HasScope(expectedScope string) bool {
+func (a *Authorizer) EnsureValidToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorization := r.Header.Get("Authorization")
+		accessToken := strings.TrimPrefix(authorization, "Bearer ")
+
+		token, claims, err := a.ParseToken(accessToken)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if !token.Valid {
+			http.Error(w, jwt.ErrTokenInvalidClaims.Error(), http.StatusUnauthorized)
+		}
+
+		if err := claims.Valid(); err != nil {
+			http.Error(w, jwt.ErrTokenInvalidClaims.Error(), http.StatusUnauthorized)
+		}
+
+		if !claims.HasScope(a.Scope) {
+			http.Error(w, errInvalidScope, http.StatusUnauthorized)
+		}
+
+		if claims.Issuer != a.Issuer {
+			http.Error(w, jwt.ErrTokenInvalidIssuer.Error(), http.StatusUnauthorized)
+		}
+
+		for _, i := range claims.Audience {
+			if i != a.Audience {
+				http.Error(w, jwt.ErrTokenInvalidAudience.Error(), http.StatusUnauthorized)
+			}
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (a *Authorizer) ParseToken(authorization string) (*jwt.Token, *MyCustomClaims, error) {
+	if len(authorization) < 1 {
+		return nil, nil, nil
+	}
+
+	accessToken := strings.TrimPrefix(authorization, "Bearer ")
+
+	claimsStruct := MyCustomClaims{}
+	token, err := jwt.ParseWithClaims(accessToken, &claimsStruct, a.Jwks.Keyfunc)
+	if err != nil {
+		return nil, nil, err
+	}
+	return token, &claimsStruct, nil
+}
+
+func (c MyCustomClaims) HasScope(expectedScope string) bool {
 	result := strings.Split(c.Scope, " ")
 	for i := range result {
 		if result[i] == expectedScope {
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jamiewhitney/auth-jwt-http
+
+go 1.18
+
+require (
+	github.com/MicahParks/keyfunc v1.5.1
+	github.com/golang-jwt/jwt/v4 v4.4.2
+)
